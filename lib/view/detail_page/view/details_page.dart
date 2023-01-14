e83be5ea28002198e7f4da3d8e40a3faf627a89f
Kaan Dinler <kaan_d_tr@hotmail.com>
2023-01-14 22:26:02 +0300

    Genre widget is added.

--- lib/view/detail_page/view/details_page.dart
+++ lib/view/detail_page/view/details_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_case_deneme_2/core/constants/constants.dart';
 import 'package:flutter_case_deneme_2/view/detail_page/components/movie_detail_widget.dart';
 import 'package:flutter_case_deneme_2/view/detail_page/view_model/movie_detail_fetch_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_case_deneme_2/core/components/flavor_config.dart';
 
 class DetailsPage extends StatelessWidget {
   final String imdbID;
@@ -21,7 +21,7 @@ class DetailsPage extends StatelessWidget {
           "${provider_movDet.movieDetailModel.title}",
           overflow: TextOverflow.ellipsis,
         ),
-        backgroundColor: kPrimaryColor,
+        backgroundColor: FlavorConfig.instance.values.theme.primaryColor,
       ),
       body: SingleChildScrollView(
         child: MovieDetailWidget(movie: provider_movDet.movieDetailModel),
