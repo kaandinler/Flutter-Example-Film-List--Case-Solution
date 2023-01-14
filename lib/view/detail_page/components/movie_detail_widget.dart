3fa3e49be1b9cd6742a72a1369cd2f6971717dea
Kaan Dinler <kaan_d_tr@hotmail.com>
2023-01-14 22:33:02 +0300

    Genre widget is added the file.

--- lib/view/detail_page/components/movie_detail_widget.dart
+++ lib/view/detail_page/components/movie_detail_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_case_deneme_2/view/detail_page/components/genre_list_widget.dart';
 import 'package:flutter_case_deneme_2/view/detail_page/components/movie_details_right_button.dart';
 import 'package:flutter_case_deneme_2/core/constants/constants.dart';
 import 'package:flutter_case_deneme_2/view/detail_page/components/movie_info_widget.dart';
@@ -39,7 +40,9 @@ class MovieDetailWidget extends StatelessWidget {
           ),
         ],
       ),
-      const SizedBox(height: kDefaultPadding / 2),
+      verticalSpaceSmall,
+      GenreListWidget(genre: movie.genre ?? "N/A"),
+      verticalSpaceSmall,
       SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: MovieInformationWidget(
@@ -51,4 +54,49 @@ class MovieDetailWidget extends StatelessWidget {
       )
     ]);
   }
+
+  // Widget buildMovieGenre(BuildContext context) {
+  //   List<String> genreList = movie.genre!.split(",");
+
+  //   return Row(
+  //     crossAxisAlignment: CrossAxisAlignment.center,
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: [
+  //       for (String word in genreList)
+  //         Padding(
+  //           padding: const EdgeInsets.all(8.0),
+  //           child: Container(
+  //             width: screenWidth(context) * 0.2,
+  //             padding: const EdgeInsets.all(kDefaultPadding / 2),
+  //             decoration: BoxDecoration(
+  //               color: Colors.white,
+  //               borderRadius: BorderRadius.circular(6),
+  //               boxShadow: [
+  //                 BoxShadow(
+  //                   offset: Offset(0, 10),
+  //                   blurRadius: 22,
+  //                   color: Colors.black.withOpacity(0.5),
+  //                 ),
+  //                 BoxShadow(
+  //                   offset: Offset(-5, -5),
+  //                   blurRadius: 11,
+  //                   color: Colors.white,
+  //                 )
+  //               ],
+  //             ),
+  //             child: Text(
+  //               word.trim(),
+  //               overflow: TextOverflow.ellipsis,
+  //               textAlign: TextAlign.center,
+  //               style: const TextStyle(
+  //                 fontSize: 16,
+  //                 fontWeight: FontWeight.bold,
+  //                 color: kTextColor,
+  //               ),
+  //             ),
+  //           ),
+  //         )
+  //     ],
+  //   );
+  // }
 }
