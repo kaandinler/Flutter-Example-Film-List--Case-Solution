bbdc333cec5e4557802be8e290d64567c666643b
Kaan Dinler <kaan_d_tr@hotmail.com>
2023-01-16 16:25:14 +0300

    unused imports are removed.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-
-import 'package:flutter_case_deneme_2/main_common.dart';
 
 void main() {
   test("status code 200", () async {
