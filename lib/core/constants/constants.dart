d95906e115da6a526be32b551b98122ef39c4232
Kaan Dinler <kaan_d_tr@hotmail.com>
2023-01-19 02:28:50 +0300

    a constant value is added.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 const kPrimaryColor = Color.fromARGB(255, 255, 88, 88);
+const kBoxShadowColor = Color.fromRGBO(143, 148, 251, .2);
 
 const double kDefaultPadding = 20.0;
 
